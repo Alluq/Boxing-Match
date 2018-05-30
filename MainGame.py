50e1757078f29d12640c1acb4cb630029705501a
AlkalineBattery <39013427+ElBattery@users.noreply.github.com>
2018-05-30 13:09:47 -0400

    Add files via upload

---
+++ MainGame.py
@@ -0,0 +1,235 @@
+import pygame
+import math
+import random
+import pygame
+
+class Text:
+
+    #initialization function
+    def init(self, screen, x, y, text, font, color):
+        self.screen = screen
+        self.ojx = float(x)
+        self.ojy = float(y)
+        self.font = font
+        self.text = text
+        self.color = color
+        self.x = self.ojx
+        self.y = self.ojy
+
+    #updates the text and draws it
+    def update(self):
+        self.screen.blit(self.font.render(str(self.text),True, self.color), (self.x,self.y))
+
+    #centers the text around the x and y point
+    def center(self):
+        self.x = self.ojx - self.font.size(str(self.text))[0]/2
+        self.y = self.ojy - self.font.size(str(self.text))[1]/2
+
+class Button:
+
+    #initialization function
+    def __init__(self, screen, rect, color, onColor, label):
+        self.screen = screen
+        self.rect = pygame.Rect(rect)
+        self.color = [color, onColor, color]
+        self.label = label
+
+    #checks if the mouse is in the button area
+    def inButton(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            return True
+        else:
+            return False
+
+    #changes cover is inButton is true
+    def hoverChange(self):
+        if self.inButton():
+            self.color[2] = self.color[1]
+        else:
+            self.color[2] = self.color[0]
+
+    #check if user has mouse button on pressed when entering the button
+    def clickButton(self):
+        if pygame.mouse.get_pressed()[0] and self.inButton():
+            return True
+        else:
+            return False
+
+    #draws button with text
+    def draw(self):
+            pygame.draw.rect(self.screen, self.color[2], self.rect)
+            self.label.ojx = self.rect[0] + self.rect[2]/2
+            self.label.ojy = self.rect[1] + self.rect[3]/2
+            self.label.center()
+            self.label.update()
+
+class Player:
+    def __init__(self, pos, speedMod, turnSpd, startDeg,surface,screen):
+        self.pos = [pos[0],pos[1]]
+        self.newPos = (pos[0] - surface.get_width()/2, pos[1] - surface.get_height()/2)
+        self.speedMod = speedMod
+        self.turnSpd = turnSpd
+        self.deg = startDeg
+        self.xSpeed = 0
+        self.ySpeed = 0
+        self.surface = surface
+        self.ojSurface = surface
+        self.surface = pygame.transform.rotate(self.surface, self.deg)
+        self.screen = screen
+        self.mask = pygame.mask.from_surface(self.surface)
+    def turn(self, event):
+        self.surface = self.ojSurface
+        if event == 0:
+            self.deg -= self.turnSpd
+        elif event == 1:
+            self.deg += self.turnSpd
+        self.surface = pygame.transform.rotate(self.surface, self.deg)
+        self.newPos = (self.pos[0] - self.surface.get_width() / 2, self.pos[1] - self.surface.get_height() / 2)
+        self.mask = pygame.mask.from_surface(self.surface)
+ #       print("cos:" + str(math.cos(math.radians(self.deg))))
+ #       print("sin:" + str(math.sin(math.radians(self.deg))))
+ #       print("deg:" + str(self.deg))
+    def accel(self):
+        if self.deg %  90 == 0:
+            if self.deg/90 % 2:
+                self.xSpeed += math.sin(math.radians(self.deg)) * self.speedMod
+            #   print("1")
+            else:
+            #  print("2")
+                self.ySpeed += math.cos(math.radians(self.deg)) * self.speedMod
+        else:
+            #  print("3")
+            self.ySpeed += math.cos(math.radians(self.deg)) * self.speedMod
+            self.xSpeed += math.sin(math.radians(self.deg)) * self.speedMod
+
+    def move(self):
+        self.pos[0] += self.xSpeed
+        self.pos[1] += self.ySpeed
+        self.newPos = (self.pos[0] - self.surface.get_width() / 2, self.pos[1] - self.surface.get_height() / 2)
+        self.mask = pygame.mask.from_surface(self.surface)
+    def draw(self):
+        self.screen.blit(self.surface, self.newPos)
+    def get_rect(self):
+        return pygame.Rect((self.newPos[0],self.newPos[1],self.surface.get_width(),self.surface.get_height()))
+
+class Match_Body(Player):
+    def __init__(self, pos, speedMod, turnSpd, startDeg,screen):
+        self.pos = [pos[0],pos[1]]
+        self.speedMod = speedMod
+        self.turnSpd = turnSpd
+        self.deg = startDeg
+        self.xSpeed = 0
+        self.ySpeed = 0
+        self.surface = pygame.image.load("body.png")
+        self.ojSurface = self.surface
+        self.surface = pygame.transform.rotate(self.surface, self.deg)
+        self.screen = screen
+        self.newPos = (pos[0] - self.surface.get_width() / 2, pos[1] - self.surface.get_height() / 2)
+        self.mask = pygame.mask.from_surface(self.surface)
+class Match_Head(Player):
+    def __init__(self, pos, speedMod, turnSpd, startDeg,screen):
+        self.pos = [pos[0],pos[1]]
+        self.speedMod = speedMod
+        self.turnSpd = turnSpd
+        self.deg = startDeg
+        self.xSpeed = 0
+        self.ySpeed = 0
+        self.surface = pygame.image.load("head.png")
+        self.ojSurface = self.surface
+        self.surface = pygame.transform.rotate(self.surface, self.deg)
+        self.screen = screen
+        self.newPos = (pos[0] - self.surface.get_width() / 2, pos[1] - self.surface.get_height() / 2)
+        self.mask = pygame.mask.from_surface(self.surface)
+class Match:
+    def __init__(self, pos, speedMod, turnSpd, startDeg,screen):
+        self.screen = screen
+        self.head = Match_Head(pos,speedMod,turnSpd, startDeg, screen)
+        self.body = Match_Body(pos, speedMod, turnSpd, startDeg, screen)
+    def turn(self, event):
+        self.head.turn(event)
+        self.body.turn(event)
+    def accel(self):
+        self.head.accel()
+        self.body.accel()
+    def draw(self):
+        self.body.draw()
+        self.head.draw()
+        new_out = []
+        new_outY = []
+        for out in self.body.mask.outline():
+            new_out.append((self.body. [0] + out[0], self.body.newPos[1] + out[1]))
+        for out in self.head.mask.outline():
+            new_outY.append((self.head.newPos[0] + out[0], self.head.newPos[1] + out[1]))
+        pygame.draw.lines(self.screen, (255, 255, 255), True, new_out)
+        pygame.draw.lines(self.head.surface,(255,255,255),True, new_outY)
+        pygame.draw.rect(self.screen, (255, 255, 255),
+                        (self.body.newPos[0], self.body.newPos[1], self.body.surface.get_width(), self.body.surface.get_height()), 1)
+    def player_collision(self,pos,othermask):
+        offset = (int(self.body.newPos[0] - pos[0]), int(self.body.newPos[1] - pos[1]))
+        if self.body.mask.overlap(othermask,offset):
+            print(offset)
+            return 1
+        elif self.head.mask.overlap(othermask,offset):
+            print(offset)
+            return 2
+        else:
+            return 0
+    def move(self):
+        self.head.move()
+        self.body.move()
+    def get_rect(self):
+        return self.body.get_rect()
+pygame.init()
+closed = False
+clock = pygame.time.Clock()
+screenRes = (640,480)
+screen = pygame.display.set_mode(screenRes)
+
+sides = [(-1 * screenRes[0], 0, screenRes[0], screenRes[1]), (0, -1 * screenRes[1], screenRes[0], screenRes[1]), (screenRes[0], 0, screenRes[0], screenRes[1]), (0, screenRes[1], screenRes[0], screenRes[1])]
+
+players = []
+
+def reset():
+    global players
+    players = [Match((screenRes[0] / 4, screenRes[1] / 2), 0.01, 1.5, 0, screen),
+               Match((screenRes[0] * 3 / 4, screenRes[1] / 2), 0.01, 1.5, 0, screen)]
+reset()
+controls = [[pygame.K_a,pygame.K_s,pygame.K_SPACE],[pygame.K_k,pygame.K_l,pygame.K_RSHIFT]]
+while not closed:
+    clock.tick(60)
+    screen.fill((0,0,0))
+    keys = pygame.key.get_pressed()
+    for i in range(len(players)):
+        if keys[controls[i][0]]:
+            players[i].turn(0)
+        if keys[controls[i][1]]:
+            players[i].turn(1)
+        if keys[controls[i][2]]:
+            players[i].accel()
+    if players[0].player_collision(players[1].head.newPos,players[1].head.mask) == 1:
+        reset()
+        print("Player 1 wins")
+    elif players[0].player_collision(players[1].head.newPos,players[1].head.mask) == 2:
+        reset()
+        print("TIE")
+    elif players[0].player_collision(players[1].body.newPos,players[1].body.mask) == 1:
+        reset()
+        print("TIEB")
+    elif players[0].player_collision(players[1].body.newPos,players[1].body.mask) == 2:
+        reset()
+        print("Player 2 wins")
+    for j in players:
+        for i in sides:
+            if j.get_rect().colliderect(i):
+                reset()
+                break
+        j.move()
+        j.draw()
+    # 1 is body 2 is head
+    pygame.display.flip()
+    for event in pygame.event.get():
+        # standard quit
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            closed = True
+pygame.quit()
